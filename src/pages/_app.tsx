c0cde60319af4cd6cd0435c6e3936e38700dadb9
lazarok09 <lazarok09@gmail.com>
2022-09-11 10:52:17 -0300

    feat(NextNProgress): add on router change ui response

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,18 @@ import { AppProps } from 'next/app';
 import '../../public/assets/fonts/styles.css';
 import { GlobalStyles } from '../styles/global-styles';
 import { SessionProvider as NextAuthProvider } from 'next-auth/react';
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextAuthProvider session={pageProps.session}>
       <ThemeProvider theme={theme}>
+        <NextNProgress
+          color={theme.colors.info}
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={10}
+        />
         <Component {...pageProps} />
         <GlobalStyles />
       </ThemeProvider>
